ef7f56eff1526db58b8aa07d71180fcdf5a8ea90
marandianil31-spec <marandianil31@gmail.com>
2026-08-29 19:29:38 +0530

    Change DropdownButtonFormField value to initialValue

--- lib/screens/artist_upload_screen.dart
+++ lib/screens/artist_upload_screen.dart
@@ -164,7 +164,7 @@ class _ArtistUploadScreenState extends State<ArtistUploadScreen> {
                 borderRadius: BorderRadius.circular(15),
               ),
               child: DropdownButtonFormField<String>(
-                value: selectedGenre,
+                initialValue: selectedGenre,
                 dropdownColor: const Color(0xFF18181F),
                 decoration: const InputDecoration(
                   prefixIcon: Icon(
