cf35937beb95beda769cdb7d105ee736ee67baac
marandianil31-spec <marandianil31@gmail.com>
2026-09-06 06:47:36 +0530

    Import album_detail_screen.dart in AlbumScreen

--- lib/screens/album_screen.dart
+++ lib/screens/album_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'music_player_screen.dart';
+import 'album_detail_screen.dart';
 
 class AlbumScreen extends StatelessWidget {
   final String albumName;
