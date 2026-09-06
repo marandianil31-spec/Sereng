88bd3aca622d71299d653cd14129549466946959
marandianil31-spec <marandianil31@gmail.com>
2026-09-06 10:49:57 +0530

    Remove unused import for album_detail_screen.dartRemove unused import

--- lib/screens/album_screen.dart
+++ lib/screens/album_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'music_player_screen.dart';
-import 'album_detail_screen.dart';
 
 class AlbumScreen extends StatelessWidget {
   final String albumName;
