a8f8bb734e6ab4e35091b5db7d98a221d94f8930
marandianil31-spec <marandianil31@gmail.com>
2026-09-12 06:30:48 +0530

    Add play function with URL validation to audio player

--- lib/services/audio_player_service.dart
+++ lib/services/audio_player_service.dart
@@ -10,27 +10,37 @@ class AudioPlayerService {
 
   AudioPlayer get player => _player;
 
+  // Play song
   Future<void> play(String url) async {
+    if (url.isEmpty) {
+      return;
+    }
+
     await _player.setUrl(url);
     await _player.play();
   }
 
+  // Pause song
   Future<void> pause() async {
     await _player.pause();
   }
 
+  // Resume song
   Future<void> resume() async {
     await _player.play();
   }
 
+  // Stop song
   Future<void> stop() async {
     await _player.stop();
   }
 
+  // Seek song position
   Future<void> seek(Duration position) async {
     await _player.seek(position);
   }
 
+  // Dispose player
   Future<void> dispose() async {
     await _player.dispose();
   }
