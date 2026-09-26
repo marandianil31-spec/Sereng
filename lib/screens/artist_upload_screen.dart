060525059dd7ada9efdf91c5f9ee590786d8cdf6
marandianil31-spec <marandianil31@gmail.com>
2026-09-26 07:16:11 +0530

    Remove unused Firebase imports
    Removed unused Firebase imports from artist_upload_screen.dart

--- lib/screens/artist_upload_screen.dart
+++ lib/screens/artist_upload_screen.dart
@@ -2,9 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ArtistUploadScreen extends StatefulWidget {
   const ArtistUploadScreen({super.key});
